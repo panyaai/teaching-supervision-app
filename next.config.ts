14e0f77092ef98240a9807d38eda5cb8bd59b45d
Dev <dev@example.com>
2026-07-30 21:16:33 +0700

    chore: setup github actions for pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = '';
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repo = '/' + process.env.GITHUB_REPOSITORY.split('/')[1];
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: repo,
+  assetPrefix: repo,
 };
 
 export default nextConfig;
